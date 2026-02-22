3fbed0eaa6ec60dc16d624bc5ddf2d5de7945bf8
Al-Amin14 <alaminj824@gmail.com>
2026-02-22 23:03:23 +0600

    Deleted Old Folder

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Learnify.Models
-{
-    public class ErrorModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
